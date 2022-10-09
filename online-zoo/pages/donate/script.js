3dec064608b2bcdb15ee3b8e1a2150c23ad5d691
Alexander Martinovich <alexander_martinovich@msn.com>
2022-10-09 16:46:10 +0300

    feat: add donate input functionality

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -6,16 +6,36 @@ const header__burger_x = document.querySelector(".header__burger_x");
 const header__burger_black_bg = document.querySelector(".header__burger_black_bg");
 
 header__burger_button.addEventListener('click', () => {
-  header__navigation.classList.add('header__nav_burger_active');
-  header__burger_black_bg.classList.add('black_bg_active');
+	header__navigation.classList.add('header__nav_burger_active');
+	header__burger_black_bg.classList.add('black_bg_active');
+	document.body.classList.add('hide_overflow');
 })
 
-header__burger_x.addEventListener('click', () => {
-  header__navigation.classList.remove('header__nav_burger_active');
-  header__burger_black_bg.classList.remove('black_bg_active');
+header__burger_x.addEventListener('click', headerBurgerClose);
+header__burger_black_bg.addEventListener('click', headerBurgerClose);
+
+function headerBurgerClose () {
+	header__navigation.classList.remove('header__nav_burger_active');
+	header__burger_black_bg.classList.remove('black_bg_active');
+	document.body.classList.remove('hide_overflow');
+}
+
+// Amount handler
+
+let radio_amount = document.querySelectorAll('.radio_amount');
+let another_amount = document.getElementById('another_amount');
+
+radio_amount.forEach((input) => {
+  input.addEventListener('input', () => {
+    another_amount.value = input.value;
+  })
 })
 
-header__burger_black_bg.addEventListener('click', () => {
-  header__navigation.classList.remove('header__nav_burger_active');
-  header__burger_black_bg.classList.remove('black_bg_active');
+another_amount.addEventListener('input', () => {
+  radio_amount.forEach((radio) => {
+    radio.checked = false;
+    if (another_amount.value == radio.value) {
+      radio.checked = true;
+    }
+  })
 })
