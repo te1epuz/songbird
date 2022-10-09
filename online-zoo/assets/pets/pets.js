adb8534ea854938d5b7fb2f385505702f51727f5
Alexander Martinovich <alexander_martinovich@msn.com>
2022-10-09 05:04:53 +0300

    fix: reupload

--- online-zoo/assets/pets/pets.js
+++ online-zoo/assets/pets/pets.js
@@ -1,48 +1,48 @@
 const petsData = [
   {
     id: '1',
-    imageSrc: '../../assets/pets/rectangle1.jpg',
+    imageSrc: '../../assets/pets/Rectangle1.jpg',
     petName: 'Giant Pandas',
     region:'Native to Southwest China',
     food: '../../assets/icons/banana-bamboo_icon.svg'
   },
   {
     id: '2',
-    imageSrc: '../../assets/pets/rectangle2.jpg',
+    imageSrc: '../../assets/pets/Rectangle2.jpg',
     petName: 'Eagles',
     region:'Native to South America',
     food: '../../assets/icons/meet-fish_icon.svg'
   },
   {
     id: '3',
-    imageSrc: '../../assets/pets/rectangle3.jpg',
+    imageSrc: '../../assets/pets/Rectangle3.jpg',
     petName: 'Gorillas',
     region:'Native to Congo',
     food: '../../assets/icons/banana-bamboo_icon.svg'
   },
   {
     id: '4',
-    imageSrc: '../../assets/pets/rectangle4.jpg',
+    imageSrc: '../../assets/pets/Rectangle4.jpg',
     petName: 'Two-toed Sloth',
     region:'Mesoamerica, South America',
     food: '../../assets/icons/banana-bamboo_icon.svg'
   },{
     id: '5',
-    imageSrc: '../../assets/pets/rectangle5.jpg',
+    imageSrc: '../../assets/pets/Rectangle5.jpg',
     petName: 'Cheetahs',
     region:'Native to Africa',
     food: '../../assets/icons/meet-fish_icon.svg'
   },
   {
     id: '6',
-    imageSrc: '../../assets/pets/rectangle6.jpg',
+    imageSrc: '../../assets/pets/Rectangle6.jpg',
     petName: 'Penguins',
     region:'Native to Antarctica',
     food: '../../assets/icons/meet-fish_icon.svg'
   },
   {
     id: '7',
-    imageSrc: '../../assets/pets/rectangle7.jpg',
+    imageSrc: '../../assets/pets/Rectangle7.jpg',
     petName: 'Alligators',
     region:'Native to Antarctica',
     food: '../../assets/icons/meet-fish_icon.svg'
