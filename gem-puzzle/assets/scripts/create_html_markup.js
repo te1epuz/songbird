3403a90aee74fe15d0a81313a3b4c49e521c94f7
Alexander Martinovich <alexander_martinovich@msn.com>
2022-10-23 19:11:20 +0300

    feat: style adjustments

--- gem-puzzle/assets/scripts/create_html_markup.js
+++ gem-puzzle/assets/scripts/create_html_markup.js
@@ -2,19 +2,14 @@ export function create_html_markup () {
 body.innerHTML =
 `<header>
     <div class="header__wrapper">
-        <nav class="header__nav">
-            <ul>
-                <li><button id="new_game_btn">new game</button>
-                    <div>
-                        <button id="new_game_less">-</button>
-                        <div id="new_game_size">4х4</div>
-                        <button id="new_game_more">+</button>
-                    </div>
-                </li>
-                <li><button id="auto_save_btn"title="save game state on page reload">auto save</button></li>
-                <li><button id="sound_btn">sound</button></li>
-            </ul>
-        </nav>
+        <div class="header__nav">
+            <div>
+                <button id="new_game_less">-</button>
+                <div id="new_game_size">4х4</div>
+                <button id="new_game_more">+</button>
+            </div>
+            <button id="new_game_btn">new game</button>
+        </div>
         <div id="win_window" class="hidden">
             <div>
                 <p id="win_message">Hooray! You solved the puzzle </br> in si##:## and N moves!</p>
@@ -30,8 +25,12 @@ body.innerHTML =
             <div>time: <span id="main__stats_time">00:00</span></div>
         </div>
         <div class="main__game"></div>
-        <div class="main__top_score">top 10 score:</div>
-        <div id="main__top_results"></div>
+        <ul class="options">
+            <li><button class="main__top_score">top 10 score</button></li>
+            <li><button id="auto_save_btn"title="save game state on page reload">auto save</button></li>
+            <li><button id="sound_btn">sound</button></li>
+        </ul>
+        <div id="main__top_results" class="hidden"></div>
     </div>
 </main>`
 }
