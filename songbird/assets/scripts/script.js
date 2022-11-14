16ccdba62f675ba899081b32ea4dd9a221c8a368
Alexander Martinovich <alexander_martinovich@msn.com>
2022-11-14 02:28:19 +0200

    feat: add answers list

--- songbird/assets/scripts/script.js
+++ songbird/assets/scripts/script.js
@@ -1,7 +1,10 @@
+import birdsData from '../birdsdata.js'
+
 const startGameBtn = document.querySelector('.main__welcome-btn');
 const navHove = document.querySelector('.header__nav-item-home');
 const navStartGameBtn = document.querySelector('.header__nav-item-start');
 const welcomeScreen = document.querySelector('.main__welcome-wrapper');
+const gameAnswers = document.querySelector('.game__answers')
 
 
 navHove.addEventListener('click', () => {
@@ -12,15 +15,25 @@ startGameBtn.addEventListener('click', startGame);
 navStartGameBtn.addEventListener('click', startGame);
 
 function startGame() {
-  console.log(111)
   welcomeScreen.classList.add('hidden-block');
   navHove.classList.remove('header__nav-item_current');
   navStartGameBtn.classList.add('header__nav-item_current');
-
   welcomeScreen.addEventListener('transitionend', () => {
     welcomeScreen.classList.add('disabled-block');
   });
 
+  let level = 0;
+  startLevel(level)
+}
 
+function startLevel(level) {
+
+  gameAnswers.innerHTML = "";
+  birdsData[level].forEach(element => {
+    let li = document.createElement('li');
+    li.className = 'game__answer';
+    li.textContent = element.name;
+    gameAnswers.append(li);
+  });
 
 }
