d6134e4d93dc24d64f2e37167bc3dfbe2ea0f9d0
Alexander Martinovich <alexander_martinovich@msn.com>
2022-11-15 00:55:39 +0200

    feat: add soundeffects

--- songbird/assets/scripts/script.js
+++ songbird/assets/scripts/script.js
@@ -20,10 +20,11 @@ const gameSelectedAnswerInfo = document.querySelector('.selected-answer__info');
 const nextLevelBtn = document.querySelector('.game__next-btn');
 const headerScore = document.querySelector('.header__score');
 const mainWinBlock = document.querySelector('.main__win');
+const mainWinText = document.querySelector('.main__win-text');
 const mainWinBtn = document.querySelector('.main__win-btn');
 
-
-
+const questionAudio = document.querySelector('.question__audio');
+const questionAudioPlayBtn = document.querySelector('.question_audio-play-btn');
 
 navHove.addEventListener('click', () => {
   window.location.href = 'index.html';
@@ -33,9 +34,9 @@ startGameBtn.addEventListener('click', startGame);
 navStartGameBtn.addEventListener('click', startGame);
 mainWinBtn.addEventListener('click', reStartGame);
 
-
 let currentLevel = 0;
 let scoreTotal = 0;
+
 function startGame() {
   welcomeScreen.classList.add('hidden-block');
   navHove.classList.remove('header__nav-item_current');
@@ -58,6 +59,7 @@ function startLevel(level) {
   console.log('correct answer: ', answerCorrect + 1);
   answersState = [0, 0, 0, 0, 0, 0]; // 0 - unclicked, 1 - clicked (wrong), 2 - clicked (right)
   updateQuestion();
+  updateQuestionAudio(birdsData[currentLevel][answerCorrect]);
   updateSelecredAnswer();
 
   gameAnswers.innerHTML = '';
@@ -68,15 +70,15 @@ function startLevel(level) {
     li.addEventListener('click', selectAnswer)
     gameAnswers.append(li);
   });
-
 }
 
 function selectAnswer (element) {
   let elementIndex = Array.from(element.target.parentNode.children).indexOf(element.target);
-  
   updateSelecredAnswer(birdsData[currentLevel][elementIndex])
   if (answersState[elementIndex] === 0) {
     if (elementIndex === answerCorrect) {
+      pauseQuestion();
+      playSoundEffect('right');
       answersState[elementIndex] = 2;
       let scoreAdd = answersState.filter(value => value === 0).length;
       scoreTotal += scoreAdd;
@@ -92,6 +94,7 @@ function selectAnswer (element) {
     } else {
       answersState[elementIndex] = 1;
       if (answersState[answerCorrect] !== 2) {
+        playSoundEffect('wrong');
         element.target.classList.add('game__answer_wrong');
       }
     }
@@ -106,6 +109,7 @@ function startNextLevel(){
   nextLevelBtn.removeEventListener('click', startNextLevel)
 
   if (currentLevel === 6){
+    playSoundEffect('victory');
     showresults();
   } else {
     gameLevels.children[currentLevel].classList.add('game__level_current');
@@ -126,6 +130,10 @@ function updateQuestion(bird = ''){
   }
 }
 
+function updateQuestionAudio(bird) {
+  questionAudio.src = bird.audio;
+}
+
 function updateSelecredAnswer(bird = ''){
   if (bird === '') {
     gameSelectedAnswerInfo.textContent = 'Послушайте плеер. Выберите птицу из списка.';
@@ -143,6 +151,14 @@ function showresults() {
 
   mainGameBlock.classList.add('disabled-block');
   mainWinBlock.classList.remove('disabled-block');
+  if (scoreTotal === 30) {
+    mainWinText.innerHTML = 'Вы набрали максимальное количество баллов (30 из 30).<br> По ТЗ кнопки рестарта тут быть не должно, поэтому можете перейти на "главную" страницу )'
+    mainWinBtn.classList.add('hidden-block');
+  } else {
+    mainWinText.innerHTML = `Вы набрали ${scoreTotal} из 30 возможных баллов. Попробуйте ещё раз!`
+  }
+ 
+
 
 }
 
@@ -150,7 +166,7 @@ function reStartGame() {
 
   mainWinBlock.classList.add('disabled-block');
   gameLevels.children[0].classList.add('game__level_current');
-
+  mainWinBtn.classList.remove('hidden-block');
   startGame();
 }
 
@@ -158,4 +174,39 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+questionAudioPlayBtn.addEventListener('click', playQuestion);
+
+function playQuestion(){
+  if (questionAudio.paused) {
+    questionAudio.play();
+  } else if (questionAudio.ended) {
+    questionAudio.currentTime = 0;
+    questionAudio.play();
+  } else {
+    questionAudio.pause();
+  }
+}
+
+function pauseQuestion(){
+  questionAudio.pause();
+}
+
+function playSoundEffect(sound) {
+  let soundEffect;
+  switch (sound) {
+    case 'right':
+      soundEffect = new Audio('assets/sounds/answer-right.mp3');
+      break;
+    case 'wrong':
+      soundEffect = new Audio('assets/sounds/answer-wrong.wav');
+      break;
+    case 'victory':
+      soundEffect = new Audio('assets/sounds/victory.wav');
+      break;
+    default:
+      console.log("no sound found for " + sound);
+  }
+  soundEffect.play();
 }
